4b2abacd2cdf3920699b9978899b9a760df4baee
yy-gx <yyang9923@gmail.com>
2024-10-27 16:52:49 -0400

    add openvla_grasp_random_object.sh

--- scripts/openvla_all_tasks_eval_distributed.sh
+++ scripts/openvla_all_tasks_eval_distributed.sh
@@ -91,72 +91,72 @@ set_5=(
   done
 ) &
 
-# Set 2
-(
-  for env_name in "${set_2[@]}"; do
-    overlay_img_list=("./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a0.png" "./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b0.png" "./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c0.png")
-    CUDA_VISIBLE_DEVICES=1 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
-    --robot google_robot_static \
-    --control-freq 3 --sim-freq 513 --max-episode-steps 200 \
-    --env-name ${env_name} --scene-name dummy_drawer \
-    --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
-    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
-    --obj-init-x-range -0.08 -0.02 3 --obj-init-y-range -0.02 0.08 3 \
-    ${EXTRA_ARGS} --overlay_img_ls "${overlay_img_list[@]}";
-  done
-) &
-
-# Set 3
-(
-  for env_name in "${set_3[@]}"; do
-    overlay_img_list=("./ManiSkill2_real2sim/data/real_inpainting/google_coke_can_real_eval_1.png")
-    CUDA_VISIBLE_DEVICES=2 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
-    --robot google_robot_static \
-    --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-    --env-name ${env_name} --scene-name ${scene_name} \
-    --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.35 -0.12 5 --obj-init-y -0.02 0.42 5 \
-    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
-    --additional-env-build-kwargs "${coke_can_options_arr[@]}" urdf_version=${urdf_version} --overlay_img_ls "${overlay_img_list[@]}";
-  done
-) &
-
-# Set 4
-(
-  for env_name in "${set_4[@]}"; do
-    overlay_img_list=("rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a0.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a1.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a2.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b0.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b1.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b2.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c0.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c1.png"
-                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c2.png")
-    CUDA_VISIBLE_DEVICES=3 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
-    --robot google_robot_static \
-    --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
-    --env-name ${env_name} --scene-name dummy_drawer \
-    --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
-    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
-    --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
-    --additional-env-build-kwargs urdf_version=${urdf_version} ${EXTRA_ARGS} --overlay_img_ls "${overlay_img_list[@]}";
-  done
-) &
-
-# Set 5
-(
-  for env_name in "${set_5[@]}"; do
-      overlay_img_list=("rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/google_move_near_real_eval_1.png")
-      CUDA_VISIBLE_DEVICES=4 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
-      --robot google_robot_static \
-      --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
-      --env-name ${env_name} --scene-name ${scene_name} \
-      --robot-init-x 0.35 0.35 1 --robot-init-y 0.21 0.21 1 --obj-variation-mode episode --obj-episode-range 0 60 \
-      --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.09 -0.09 1 \
-      --additional-env-build-kwargs urdf_version=${urdf_version} \
-      --additional-env-save-tags baked_except_bpb_orange --overlay_img_ls "${overlay_img_list[@]}";
-  done
-) &
+## Set 2
+#(
+#  for env_name in "${set_2[@]}"; do
+#    overlay_img_list=("./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a0.png" "./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b0.png" "./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c0.png")
+#    CUDA_VISIBLE_DEVICES=1 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
+#    --robot google_robot_static \
+#    --control-freq 3 --sim-freq 513 --max-episode-steps 200 \
+#    --env-name ${env_name} --scene-name dummy_drawer \
+#    --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
+#    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
+#    --obj-init-x-range -0.08 -0.02 3 --obj-init-y-range -0.02 0.08 3 \
+#    ${EXTRA_ARGS} --overlay_img_ls "${overlay_img_list[@]}";
+#  done
+#) &
+#
+## Set 3
+#(
+#  for env_name in "${set_3[@]}"; do
+#    overlay_img_list=("./ManiSkill2_real2sim/data/real_inpainting/google_coke_can_real_eval_1.png")
+#    CUDA_VISIBLE_DEVICES=2 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
+#    --robot google_robot_static \
+#    --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+#    --env-name ${env_name} --scene-name ${scene_name} \
+#    --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-init-x -0.35 -0.12 5 --obj-init-y -0.02 0.42 5 \
+#    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 \
+#    --additional-env-build-kwargs "${coke_can_options_arr[@]}" urdf_version=${urdf_version} --overlay_img_ls "${overlay_img_list[@]}";
+#  done
+#) &
+#
+## Set 4
+#(
+#  for env_name in "${set_4[@]}"; do
+#    overlay_img_list=("rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a0.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a1.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_a2.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b0.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b1.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_b2.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c0.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c1.png"
+#                      "rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/open_drawer_c2.png")
+#    CUDA_VISIBLE_DEVICES=3 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
+#    --robot google_robot_static \
+#    --control-freq 3 --sim-freq 513 --max-episode-steps 113 \
+#    --env-name ${env_name} --scene-name dummy_drawer \
+#    --robot-init-x 0.644 0.644 1 --robot-init-y -0.179 -0.179 1 \
+#    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.03 -0.03 1 \
+#    --obj-init-x-range 0 0 1 --obj-init-y-range 0 0 1 \
+#    --additional-env-build-kwargs urdf_version=${urdf_version} ${EXTRA_ARGS} --overlay_img_ls "${overlay_img_list[@]}";
+#  done
+#) &
+#
+## Set 5
+#(
+#  for env_name in "${set_5[@]}"; do
+#      overlay_img_list=("rgb_overlay_path=./ManiSkill2_real2sim/data/real_inpainting/google_move_near_real_eval_1.png")
+#      CUDA_VISIBLE_DEVICES=4 python simpler_env/main_inference.py --policy-model openvla --ckpt-path ${ckpt_path} \
+#      --robot google_robot_static \
+#      --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
+#      --env-name ${env_name} --scene-name ${scene_name} \
+#      --robot-init-x 0.35 0.35 1 --robot-init-y 0.21 0.21 1 --obj-variation-mode episode --obj-episode-range 0 60 \
+#      --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 -0.09 -0.09 1 \
+#      --additional-env-build-kwargs urdf_version=${urdf_version} \
+#      --additional-env-save-tags baked_except_bpb_orange --overlay_img_ls "${overlay_img_list[@]}";
+#  done
+#) &
 
 # Wait for all background processes to finish
 wait
