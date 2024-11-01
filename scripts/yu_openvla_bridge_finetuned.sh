e738ac0a86729df3152752063e5565f7a54bc75b
yy-gx <yyang9923@gmail.com>
2024-10-31 20:09:15 -0400

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_bridge_finetuned.sh
+++ scripts/yu_openvla_bridge_finetuned.sh
@@ -1,14 +1,14 @@
-gpu_id=1
+gpu_id=2
 policy_model=openvla
 ckpt_path="openvla/openvla-7b"
 
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.3/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.0.3-v2/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--4000_chkpt")
 
-#carrot
-declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.4/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt")
+##carrot
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.4/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt")
 
-##spoon
-#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.5/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt")
+#spoon
+declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.5/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt")
 
 scene_name=bridge_table_1_v1
 robot=widowx
@@ -18,13 +18,13 @@ robot_init_y=0.028
 
 #yy: add for loop ckpt
 for ckpt_path in "${arr[@]}"; do
-  CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
-    --robot ${robot} --policy-setup widowx_bridge \
-    --control-freq 5 --sim-freq 500 --max-episode-steps 60 \
-    --env-name PutCarrotOnPlateInScene-v0 --scene-name ${scene_name} \
-    --rgb-overlay-path ${rgb_overlay_path} \
-    --robot-init-x ${robot_init_x} ${robot_init_x} 1 --robot-init-y ${robot_init_y} ${robot_init_y} 1 --obj-variation-mode episode --obj-episode-range 0 24 \
-    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 --instruction "Pick up the carrot and place it on the plate";
+#  CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
+#    --robot ${robot} --policy-setup widowx_bridge \
+#    --control-freq 5 --sim-freq 500 --max-episode-steps 60 \
+#    --env-name PutCarrotOnPlateInScene-v0 --scene-name ${scene_name} \
+#    --rgb-overlay-path ${rgb_overlay_path} \
+#    --robot-init-x ${robot_init_x} ${robot_init_x} 1 --robot-init-y ${robot_init_y} ${robot_init_y} 1 --obj-variation-mode episode --obj-episode-range 0 24 \
+#    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 --instruction "Pick up the carrot and place it on the plate";
 
 #  CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
 #    --robot ${robot} --policy-setup widowx_bridge \
@@ -34,13 +34,13 @@ for ckpt_path in "${arr[@]}"; do
 #    --robot-init-x ${robot_init_x} ${robot_init_x} 1 --robot-init-y ${robot_init_y} ${robot_init_y} 1 --obj-variation-mode episode --obj-episode-range 0 24 \
 #    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1;
 
-#  CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
-#    --robot ${robot} --policy-setup widowx_bridge \
-#    --control-freq 5 --sim-freq 500 --max-episode-steps 60 \
-#    --env-name PutSpoonOnTableClothInScene-v0 --scene-name ${scene_name} \
-#    --rgb-overlay-path ${rgb_overlay_path} \
-#    --robot-init-x ${robot_init_x} ${robot_init_x} 1 --robot-init-y ${robot_init_y} ${robot_init_y} 1 --obj-variation-mode episode --obj-episode-range 0 24 \
-#    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 --instruction "Pick up the spoon and place it on the cloth";
+  CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
+    --robot ${robot} --policy-setup widowx_bridge \
+    --control-freq 5 --sim-freq 500 --max-episode-steps 60 \
+    --env-name PutSpoonOnTableClothInScene-v0 --scene-name ${scene_name} \
+    --rgb-overlay-path ${rgb_overlay_path} \
+    --robot-init-x ${robot_init_x} ${robot_init_x} 1 --robot-init-y ${robot_init_y} ${robot_init_y} 1 --obj-variation-mode episode --obj-episode-range 0 24 \
+    --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 --instruction "Pick up the spoon and place it on the cloth";
 done
 
 
