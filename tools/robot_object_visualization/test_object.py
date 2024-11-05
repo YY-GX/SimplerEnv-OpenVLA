4fe16eef49441041de02179cae314434000bc923
yy-gx <yyang9923@gmail.com>
2024-11-05 16:02:22 -0500

    add openvla_grasp_random_object.sh

--- tools/robot_object_visualization/test_object.py
+++ tools/robot_object_visualization/test_object.py
@@ -35,22 +35,22 @@ def build_actor(
 
 
 def demo(model_dir):
-    engine = sapien.Engine()
-    renderer = sapien.SapienRenderer()
-    engine.set_renderer(renderer)
-
-    scene_config = sapien.SceneConfig()
-    scene = engine.create_scene(scene_config)
-    scene.set_timestep(1 / 500.0)
-    scene.add_ground(0)
-
-    scene.set_ambient_light([0.5, 0.5, 0.5])
-    scene.add_directional_light([0, 1, -1], [0.5, 0.5, 0.5])
-
-    viewer = Viewer(renderer)
-    viewer.set_scene(scene)
-    viewer.set_camera_xyz(x=-2, y=0, z=1)
-    viewer.set_camera_rpy(r=0, p=-0.3, y=0)
+    # engine = sapien.Engine()
+    # renderer = sapien.SapienRenderer()
+    # engine.set_renderer(renderer)
+    #
+    # scene_config = sapien.SceneConfig()
+    # scene = engine.create_scene(scene_config)
+    # scene.set_timestep(1 / 500.0)
+    # scene.add_ground(0)
+    #
+    # scene.set_ambient_light([0.5, 0.5, 0.5])
+    # scene.add_directional_light([0, 1, -1], [0.5, 0.5, 0.5])
+    #
+    # viewer = Viewer(renderer)
+    # viewer.set_scene(scene)
+    # viewer.set_camera_xyz(x=-2, y=0, z=1)
+    # viewer.set_camera_rpy(r=0, p=-0.3, y=0)
 
     obj = build_actor(
         model_dir,
@@ -61,23 +61,23 @@ def demo(model_dir):
     )
     obj.set_pose(sapien.Pose([0, 0, 1], [1, 0, 0, 0]))
 
-    camera = scene.add_camera(
-        name="camera",
-        width=int(848),
-        height=int(480),
-        fovy=np.deg2rad(78.0),  # D435 fovy
-        near=0.1,
-        far=10.0,
-    )
-    camera.set_focal_lengths(605.12, 604.91)
-    camera.set_principal_point(424.59, 236.67)
-    camera.set_parent(parent=obj, keep_pose=False)
-
-    while not viewer.closed:
-        for _ in range(4):  # render every 4 steps
-            scene.step()
-        scene.update_render()
-        viewer.render()
+    # camera = scene.add_camera(
+    #     name="camera",
+    #     width=int(848),
+    #     height=int(480),
+    #     fovy=np.deg2rad(78.0),  # D435 fovy
+    #     near=0.1,
+    #     far=10.0,
+    # )
+    # camera.set_focal_lengths(605.12, 604.91)
+    # camera.set_principal_point(424.59, 236.67)
+    # camera.set_parent(parent=obj, keep_pose=False)
+    #
+    # while not viewer.closed:
+    #     for _ in range(4):  # render every 4 steps
+    #         scene.step()
+    #     scene.update_render()
+    #     viewer.render()
 
 
 def main():
