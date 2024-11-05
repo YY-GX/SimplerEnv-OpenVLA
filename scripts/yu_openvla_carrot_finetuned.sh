2fc73a32813000d641c6e9d110ac13a382f904da
yy-gx <yyang9923@gmail.com>
2024-11-04 19:30:10 -0500

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_carrot_finetuned.sh
+++ scripts/yu_openvla_carrot_finetuned.sh
@@ -9,7 +9,8 @@ policy_model=openvla
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.1/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--20000_chkpt")
 #All
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2-bs64/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--100000_chkpt")
-declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--100000_chkpt")
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--100000_chkpt")
+declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--5000_chkpt")
 
 scene_name=bridge_table_1_v1
 robot=widowx
