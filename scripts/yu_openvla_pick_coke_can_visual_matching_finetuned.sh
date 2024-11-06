247348893fef8bb036c6c1fb0cc5391c27726470
yy-gx <yyang9923@gmail.com>
2024-11-06 12:43:51 -0500

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_pick_coke_can_visual_matching_finetuned.sh
+++ scripts/yu_openvla_pick_coke_can_visual_matching_finetuned.sh
@@ -1,14 +1,14 @@
 
 
-gpu_id=5
+gpu_id=6
 
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.0.3/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--image_aug--1000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.0.3-v2/openvla-7b+droid+b8+lr-0.0001+lora-r32+dropout-0.0--4000_chkpt")
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2-bs64/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--100000_chkpt")
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--100000_chkpt")
 # nov 6 - coke
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.8/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--1000_chkpt")
-declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.8/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--3000_chkpt")
-#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.8/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--10000_chkpt")
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.8/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--3000_chkpt")
+declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.8/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--10000_chkpt")
 
 
 # lr_switch=laying horizontally but flipped left-right to match real eval; upright=standing; laid_vertically=laying vertically
