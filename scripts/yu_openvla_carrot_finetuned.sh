4b29ead71349048bc474e9659cf42f2c7fdbcadd
yy-gx <yyang9923@gmail.com>
2024-11-06 17:44:01 -0500

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_carrot_finetuned.sh
+++ scripts/yu_openvla_carrot_finetuned.sh
@@ -12,7 +12,12 @@ policy_model=openvla
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--100000_chkpt")
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--5000_chkpt")
 #declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--1000_chkpt")
-declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--3000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--2000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--30000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.2-bs64/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--15000_chkpt")
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--3000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.0/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--2000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.2/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--30000_chkpt" "/mnt/bum/yufang/projects/openvla/runs/1.1.2-bs64/openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--15000_chkpt")
+
+#Carrot
+declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.5-v2/carrot-1.1.5-v2-openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--1000_chkpt")
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.5-v2/carrot-1.1.5-v2-openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--3000_chkpt")
+#declare -a arr=("/mnt/bum/yufang/projects/openvla/runs/1.1.5-v2/carrot-1.1.5-v2-openvla-7b+droid+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--5000_chkpt")
 
 scene_name=bridge_table_1_v1
 robot=widowx
