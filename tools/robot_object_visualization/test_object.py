eaef3ad3b1c0e27f6372f83a41dcdaf465b347fa
yy-gx <yyang9923@gmail.com>
2024-11-05 21:15:40 -0500

    add openvla_grasp_random_object.sh

--- tools/robot_object_visualization/test_object.py
+++ tools/robot_object_visualization/test_object.py
@@ -25,9 +25,15 @@ def build_actor(
         density=density,
     )
 
-    visual_file = str(model_dir / "textured.dae")
-    if not Path(visual_file).exists():
-        visual_file = str(model_dir / "textured.glb")
+    # visual_file = str(model_dir / "textured.dae")
+    # if not Path(visual_file).exists():
+    #     visual_file = str(model_dir / "textured.glb")
+
+    visual_file = str(model_dir / "textured.obj")
+    if not os.path.exists(visual_file):
+        visual_file = str(model_dir / "textured.dae")
+        if not os.path.exists(visual_file):
+            visual_file = str(model_dir / "textured.glb")
     builder.add_visual_from_file(filename=visual_file, scale=[scale] * 3)
 
     actor = builder.build()
