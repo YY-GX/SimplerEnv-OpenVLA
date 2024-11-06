44a79ff7b3869b41aa55335e3ff524c6bcf2881a
yy-gx <yyang9923@gmail.com>
2024-11-05 22:31:31 -0500

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_debug_new_obj.sh
+++ scripts/yu_openvla_debug_new_obj.sh
@@ -14,10 +14,10 @@ for seed in "${seeds[@]}"; do
   for ckpt_path in "${arr[@]}"; do
     CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
       --robot google_robot_static \
-      --control-freq 3 --sim-freq 513 --max-episode-steps 5 \
+      --control-freq 3 --sim-freq 513 --max-episode-steps 80 \
       --env-name GraspSingleCupInScene-v0 --scene-name ${scene_name} \
       --rgb-overlay-path ${rgb_overlay_path} \
-      --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-variation-mode episode --obj-episode-range 0 1 \
+      --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-variation-mode episode --obj-episode-range 0 5 \
       --robot-init-rot-quat-center 0 0 0 1 --robot-init-rot-rpy-range 0 0 1 0 0 1 0 0 1 --instruction "Pick up the cup" \
       --seed "$seed" --additional-env-build-kwargs urdf_version="recolor_tabletop_visual_matching_1";
   done
