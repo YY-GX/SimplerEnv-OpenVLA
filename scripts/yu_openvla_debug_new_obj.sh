02c81da3e9c22892df777328f7a35e5a68558cf5
yy-gx <yyang9923@gmail.com>
2024-11-05 22:18:06 -0500

    add openvla_grasp_random_object.sh

--- scripts/yu_openvla_debug_new_obj.sh
+++ scripts/yu_openvla_debug_new_obj.sh
@@ -14,7 +14,7 @@ for seed in "${seeds[@]}"; do
   for ckpt_path in "${arr[@]}"; do
     CUDA_VISIBLE_DEVICES=${gpu_id} python simpler_env/main_inference.py --policy-model ${policy_model} --ckpt-path ${ckpt_path} \
       --robot google_robot_static \
-      --control-freq 3 --sim-freq 513 --max-episode-steps 10 \
+      --control-freq 3 --sim-freq 513 --max-episode-steps 5 \
       --env-name GraspSingleCupInScene-v0 --scene-name ${scene_name} \
       --rgb-overlay-path ${rgb_overlay_path} \
       --robot-init-x 0.35 0.35 1 --robot-init-y 0.20 0.20 1 --obj-variation-mode episode --obj-episode-range 0 1 \
